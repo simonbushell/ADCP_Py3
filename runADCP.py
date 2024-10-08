553a980d7c144b453c9ffb33e304715c089e7cb9
Simon Bushell <simon.bushell@orbitdiscovery.com>
2024-10-08 10:57:19 +0100

    fixed runADCP.py to make Py3 compatible

--- runADCP.py
+++ runADCP.py
@@ -31,7 +31,6 @@
 # $Id: runADFR.py,v 1.36 2017/11/15 00:41:43 sanner Exp $
 #
 #
-
 import os, sys, numpy, platform, datetime, tempfile, shutil, random, tarfile, pickle
 from glob import glob
 
@@ -44,14 +43,14 @@ class runADCP:
 
     def myexit(self):
         if self.targetFile is not None:
-            print "clean up unzipped map files"
+            print("clean up unzipped map files")
             try:
-                shutil.rmtree('./tmp_%s'%self.jobName)
+                shutil.rmtree('./tmp_%s' % self.jobName)
             except OSError:
                 pass        
-            for element in ['C','A','SA','N','NA','OA','HD','d','e']:
-                if os.path.isfile('rigidReceptor.%s.map'%element):
-                    os.remove('rigidReceptor.%s.map'%element)
+            for element in ['C', 'A', 'SA', 'N', 'NA', 'OA', 'HD', 'd', 'e']:
+                if os.path.isfile('rigidReceptor.%s.map' % element):
+                    os.remove('rigidReceptor.%s.map' % element)
             if os.path.isfile('transpoints'):
                 os.remove('transpoints')
             if os.path.isfile('translationPoints.npy'):
@@ -69,10 +68,10 @@ class runADCP:
         _platform = system_info[0]
 
         if _platform == 'Windows':
-            self.shell=False
+            self.shell = False
             self._argv = ['./adcp -t 2']
         else:
-            self.shell=True
+            self.shell = True
             self._argv = ['./adcp_Linux-x86_64 -t 2']
 
         # modify here
@@ -81,9 +80,7 @@ class runADCP:
 
         self.completedJobs = 0    
         self.numberOfJobs = 0
-        self.outputBaseName = None # a folder with that name will be crated to store log files and ligands
-                                   # a _summary.dlg file will be create with this name too
-                                   # is specified using -o on the command line
+        self.outputBaseName = None
         self.jobName = 'NoName'
         self.targetFile = None
      
@@ -95,7 +92,7 @@ class runADCP:
         dataDict = {}
         
         seed = None
-        rncpu= None
+        rncpu = None
         nbRuns = 50
         numSteps = 2500000
         jobName = 'NoName'
@@ -106,26 +103,26 @@ class runADCP:
 
         if rncpu is None:
             ncores = self.ncpu
-            self.myprint( 'Detected %d cores, using %d cores'%(self.ncpu, ncores))
+            self.myprint('Detected %d cores, using %d cores' % (self.ncpu, ncores))
         else:
-            assert rncpu > 0, "ERROR: maxCores a positive number, got %d"%rncpu
+            assert rncpu > 0, "ERROR: maxCores a positive number, got %d" % rncpu
             ncores = min(self.ncpu, rncpu)
-            self.myprint( 'Detected %d cores, request %d cores, using %d cores'%(self.ncpu, rncpu, ncores))
+            self.myprint('Detected %d cores, request %d cores, using %d cores' % (self.ncpu, rncpu, ncores))
 
         if kw['nbRuns'] is not None:
             self.nbRuns = nbRuns = kw.pop('nbRuns')
 
         self.numberOfJobs = nbRuns
-        self._jobStatus =  [None]*nbRuns        
+        self._jobStatus = [None] * nbRuns        
 
         seed = kw.pop('seedValue')
 
         if seed is None:
-            seed = str(random.randint(1,999999))
+            seed = str(random.randint(1, 999999))
 
         # check ramaprob.data file
         if not os.path.isfile("ramaprob.data"):
-            print "ERROR: cannot find probability data for ramachandra plot"
+            print("ERROR: cannot find probability data for ramachandra plot")
             self.myexit()
 
         if kw['jobName'] is not None:
@@ -134,47 +131,41 @@ class runADCP:
         # if target is zip file unzip and replace cmdline arguments
         self.targetFile = targetFile = kw.pop('target')
         if targetFile is None and not os.path.isfile("transpoints"):
-            print "ERROR: no receptor files found"
+            print("ERROR: no receptor files found")
             self.myexit()
+
         # if transpoints file does not exists
         elif targetFile is not None:
-            # unzip mapsFile
             import zipfile
             with zipfile.ZipFile(targetFile, 'r') as zip_ref:
-                zip_ref.extractall('./tmp_%s/'%jobName)
-            for element in ['C','A','SA','N','NA','OA','HD','d','e']:
+                zip_ref.extractall('./tmp_%s/' % jobName)
+            for element in ['C', 'A', 'SA', 'N', 'NA', 'OA', 'HD', 'd', 'e']:
                 try:
-                #print("warningsaqd")
-                    shutil.copy(os.path.join('./tmp_%s/'%jobName,targetFile[:-4],'rigidReceptor.%s.map'%element),os.getcwd())
+                    shutil.copy(os.path.join('./tmp_%s/' % jobName, targetFile[:-4], 'rigidReceptor.%s.map' % element), os.getcwd())
                 except IOError:
-                    print "WARNING: cannot locate map file for %s"%element
-            shutil.copy(os.path.join('./tmp_%s/'%jobName,targetFile[:-4],'translationPoints.npy'),os.getcwd())
+                    print("WARNING: cannot locate map file for %s" % element)
+            shutil.copy(os.path.join('./tmp_%s/' % jobName, targetFile[:-4], 'translationPoints.npy'), os.getcwd())
             ttt = numpy.load('translationPoints.npy')
-            fff = open('transpoints','w+')
-            fff.write('%s\n'%len(ttt))
-            numpy.savetxt(fff,ttt,fmt='%7.3f')
-            fff.close()
+            with open('transpoints', 'w+') as fff:
+                fff.write('%s\n' % len(ttt))
+                numpy.savetxt(fff, ttt, fmt='%7.3f')
         else:
-            for element in ['C','A','SA','N','NA','OA','HD','d','e']:
-                if not os.path.isfile("rigidReceptor.%s.map"%element):
-                    print "WARNING: cannot locate map file rigidReceptor.%s.map"%element
+            for element in ['C', 'A', 'SA', 'N', 'NA', 'OA', 'HD', 'd', 'e']:
+                if not os.path.isfile("rigidReceptor.%s.map" % element):
+                    print("WARNING: cannot locate map file rigidReceptor.%s.map" % element)
 
+        with open('con', 'w+') as fff:
+            fff.write('1\n')
 
-        fff = open('con','w+')
-        fff.write('1\n')
-        fff.close()
-
-
-
-
+        
         #check overwriting files
         for i in range(nbRuns):
-            if os.path.isfile('%s_%d.pdb'%(jobName,i+1)):
+            if os.path.isfile('%s_%d.pdb' % (jobName, i + 1)):
                 if not kw['overwriteFiles']:
-                    print "ERROR: output file exists %s_%d.pdb"%(jobName,i+1)
+                    print("ERROR: output file exists %s_%d.pdb" % (jobName, i + 1))
                     self.myexit()
                 else:
-                    print "Warning: overwriting output file %s_%d.pdb"%(jobName,i+1)
+                    print("Warning: overwriting output file %s_%d.pdb" % (jobName, i + 1))
 
         self.dryRun = kw.pop('dryRun')
 
@@ -183,21 +174,21 @@ class runADCP:
 
         if kw['sequence'] is None:
             if kw['input'] is None or kw['input'][-3:] != 'pdb':
-                print "ERROR: no input for peptide found"
+                print("ERROR: no input for peptide found")
                 self.myexit()
             else:
                 argv.append('-f')
-                argv.append('%s'%kw['input'])                
+                argv.append('%s' % kw['input'])                
         else:
-                argv.append('%s'%kw['sequence'])
+            argv.append('%s' % kw['sequence'])
 
-        # set up the length for each run, 25M as default
+        # set up the length for each run, 25M as default        
         argv.append('-r')
         if kw['numSteps'] is not None:
             numSteps = kw['numSteps']        
-        argv.append('1x%s'%numSteps)
+        argv.append('1x%s' % numSteps)
 
-        # set up other options for ADCP
+        # set up other options for ADCP        
         ADCPDefaultOptions = "-p Bias=NULL,external=5,con,1.0,1.0"
         if kw['cyclic']:
             ADCPDefaultOptions += ",external2=4,con14,1.0,1.0"
@@ -208,68 +199,56 @@ class runADCP:
 
         # add arguments that will be set during the loop submitting jobs
         # for seed jubNum and outputName
-        argv.extend(['-s', '-1', '-o', jobName,' '])
-        jobs = {} # key will be process until process.poll() is not None (i.e. finished)
+        argv.extend(['-s', '-1', '-o', jobName, ' '])
+        jobs = {}
 
         from time import time, sleep
         t0 = time()
-        runStatus = [None]*(nbRuns)
+        runStatus = [None] * (nbRuns)
 
-        runEnergies = [999.]*(nbRuns)
+        runEnergies = [999.] * (nbRuns)
         procToRun = {}
-        nbStart = 0 # number of started runs
-        nbDone = 0 # number of completed runs
+        nbStart = 0
+        nbDone = 0
 
-        self.myprint( "Performing search (%d ADCP runs with %d steps each) ..."%
-                      (nbRuns, numSteps))
-        print "0%   10   20   30   40   50   60   70   80   90   100%"
-        print "|----|----|----|----|----|----|----|----|----|----|"
+        self.myprint("Performing search (%d ADCP runs with %d steps each) ..." % (nbRuns, numSteps))
+        print("0%   10   20   30   40   50   60   70   80   90   100%")
+        print("|----|----|----|----|----|----|----|----|----|----|")
 
-
-
-        # submit the first set of jobs
-        for jobNum in range(1,min(nbRuns,ncores)+1):
-            # overwrite seed
+        # submit the first set of jobs        
+        for jobNum in range(1, min(nbRuns, ncores) + 1):
             if seed == -1:
-                argv[-4] = str(random.randint(1,999999))
+                argv[-4] = str(random.randint(1, 999999))
             else:
-                argv[-4] = str(seed+jobNum-1)
-            # overwrite jobNum
-            argv[-2] = '%s_%d.pdb'%(jobName,jobNum)
-            argv[-1] = '> %s_%d.out 2>&1'%(jobName,jobNum)
+                argv[-4] = str(seed + jobNum - 1)
+            argv[-2] = '%s_%d.pdb' % (jobName, jobNum)
+            argv[-1] = '> %s_%d.out 2>&1' % (jobName, jobNum)
             if self.dryRun:
-                print '/n*************** command ***************************\n'
-                print ' '.join(argv)
-                print
+                print('/n*************** command ***************************\n')
+                print(' '.join(argv))
+                print()
                 self.myexit()
                 
             process = subprocess.Popen(' '.join(argv),
                                        stdout=subprocess.PIPE , 
                                        stderr=subprocess.PIPE, 
                                        bufsize = 1, shell=self.shell, cwd=os.getcwd())
-            procToRun[process] = jobNum-1
+            procToRun[process] = jobNum - 1
             nbStart += 1
 
         # check for completion and start new runs ntil we are done
         while nbDone < nbRuns:
-            # check running processes
             for proc, jnum in procToRun.items():
-                #import pdb;pdb.set_trace()
-                if proc.poll() is not None: # process finished
-                    if proc.returncode !=0:
-                        runStatus[jnum] = ('Error', '%s%04d'%(jobName, jnum+1))
+                if proc.poll() is not None:
+                    if proc.returncode != 0:
+                        runStatus[jnum] = ('Error', '%s%04d' % (jobName, jnum + 1))
                         error = '\n'.join(runStatus[jnum][1])
                         status = 'FAILED'
-                        #self.myprint( jnum, 'ENDED WITH ERROR')
-                        #print '%d err'%jnum
                     else:
                         status = 'OK'
                         error = ''
-                        runStatus[jnum] = ('OKAY', '%s%04d'%(jobName, jnum+1))
-                        #self.myprint( jnum, 'ENDED OK')
-                        #print '%d ok'%jnum
-                        #import pdb;pdb.set_trace()
-                        f = open('%s_%d.out'%(jobName,jnum+1))
+                        runStatus[jnum] = ('OKAY', '%s%04d' % (jobName, jnum + 1))
+                        f = open('%s_%d.out' % (jobName, jnum + 1))
                         lines = f.readlines()
                         f.close()
                         for ln in lines:
@@ -277,26 +256,22 @@ class runADCP:
                                 runEnergies[jnum] = float(ln.rstrip().split()[3])
 
                     nbDone += 1
-                    # remove process
                     del procToRun[proc]
 
-                    self._jobStatus[jobNum-1] = 2
+                    self._jobStatus[jnum] = 2
                     self.completedJobs += 1
-                    percent = float(self.completedJobs)/self.numberOfJobs
-                    sys.stdout.write('%s\r' % ('*'*int(50*percent)))
+                    percent = float(self.completedJobs) / self.numberOfJobs
+                    sys.stdout.write('%s\r' % ('*' * int(50 * percent)))
                     sys.stdout.flush()
 
                     if nbStart < nbRuns:
-                        # start new one
                         jobNum += 1
                         if seed == -1:
-                            argv[-4] = str(random.randint(1,999999))
+                            argv[-4] = str(random.randint(1, 999999))
                         else:
-                            argv[-4] = str(seed+jobNum-1)
-                        # overwrite jobNum
-                        argv[-2] = '%s_%d.pdb'%(jobName,jobNum)
-                        argv[-1] = '> %s_%d.out 2>&1'%(jobName,jobNum)
-                        # remove output file in case it exists
+                            argv[-4] = str(seed + jobNum - 1)
+                        argv[-2] = '%s_%d.pdb' % (jobName, jobNum)
+                        argv[-1] = '> %s_%d.out 2>&1' % (jobName, jobNum)
                         try:
                             os.remove(argv[-1])
                         except OSError:
@@ -306,39 +281,38 @@ class runADCP:
                                                    stdout=subprocess.PIPE , 
                                                    stderr=subprocess.PIPE, 
                                                    bufsize = 1, shell=self.shell, cwd=os.getcwd())
-                        procToRun[process] = jobNum-1
+                        procToRun[process] = jobNum - 1
                         nbStart += 1
             sleep(1)
 
-        dt = time()-t0
-        h,m,s = str(datetime.timedelta(seconds=dt)).split(':')
-        self.myprint( 'Docking performed in %.2f seconds, i.e. %s hours %s minutes %s seconds '%(dt, h, m, s))
+        dt = time() - t0
+        h, m, s = str(datetime.timedelta(seconds=dt)).split(':')
+        self.myprint('Docking performed in %.2f seconds, i.e. %s hours %s minutes %s seconds ' % (dt, h, m, s))
         
         #write out energy for top 5 solutions
         sort_index = numpy.argsort(runEnergies)
         for i in range(5):
-            self.myprint('No. %d energy found is %3.1f kcal/mol at %s_%d.pdb '%(i+1, runEnergies[sort_index[i]]*0.59219, jobName, sort_index[i]+1))
+            self.myprint('No. %d energy found is %3.1f kcal/mol at %s_%d.pdb ' % (i + 1, runEnergies[sort_index[i]] * 0.59219, jobName, sort_index[i] + 1))
         self.myexit()
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
     #from ADFR.utils.runADFR import runADFR
     #from ADFR.utils.optParser import ArgParser
 
     import argparse
     parser = argparse.ArgumentParser(description='AutoDock CrankPep', 
-                                     usage="usage: python %(prog)s -s GaRyMiChEL -t rec.trg -o output",
-                                     version="%prog 0.1")
+                                     usage="usage: python %(prog)s -s GaRyMiChEL -t rec.trg -o output")
 
-    parser.add_argument("-s", "--sequence",dest="sequence")
-    parser.add_argument("-i", "--input",dest="input")
-    parser.add_argument("-t", "--target",dest="target")
+    parser.add_argument("-s", "--sequence", dest="sequence")
+    parser.add_argument("-i", "--input", dest="input")
+    parser.add_argument("-t", "--target", dest="target")
     parser.add_argument("-n", "--numSteps", type=int,
                              default=2500000, dest="numSteps")
     parser.add_argument("-N", "--nbRuns", type=int,
                              default=50, dest="nbRuns")
     parser.add_argument("-c", "--maxCores", type=int, dest="maxCores")
-    parser.add_argument("-o", "--jobName",dest="jobName")
+    parser.add_argument("-o", "--jobName", dest="jobName")
     parser.add_argument(
             '-y', "--dryRun", dest="dryRun", action="store_true",
             default=False,
